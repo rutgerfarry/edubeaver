9dc9945142fb75d1ca756a586e2b73cb340d268f
Rutger Farry <rutgerfarry@gmail.com>
2015-04-07 09:17:21 -0700

    Split parsing into functions for getClassInfo()

--- searchParse.js
+++ searchParse.js
@@ -27,37 +27,65 @@ function getClassLinks (url) {
 }
 
 function getClassInfo (baseURL, classURLs) {
-	var classURL = baseURL + classURLs[320];
+	var classURL = baseURL + classURLs[321];
 
 	request(classURL, function parseClassPage (error, response, body) {
 		if (!error && response.statusCode === 200) {
 			$ = cheerio.load(body);
-			var name = $("h3").text().trim();
-			var desc = $("#aspnetForm").first()
-									   .clone()
-									   .children()
-									   .remove()
-									   .end()
-									   .text()
-									   .trim();
-			desc = trimNewlines(desc);
-			var abbr = name.match(/^[A-Z]{1,4}\s[0-9]{2,3}/);
-			var credits = name.match(/\(([^\)]+)\)/i)[1];
-
-			name = name.replace(/(^[A-Z]{1,4}\s[0-9]{2,3})/, '');
-			name = name.replace(/\(([^\)]+)\)/i, '');
-			name = name.replace(/[^\w\s]/gi, '');
-			name = name.replace(/\r?\n|\r|\t/g, '');
-			name = name.replace(/\ {2,}/g, '');
+			var title   = parseTitle($),
+				abbr    = parseAbbr($),
+				credits = parseCredits($),
+				desc    = parseDesc($);
+			// var desc = $("#aspnetForm").first()
+			// 						   .clone()
+			// 						   .children()
+			// 						   .remove()
+			// 						   .end()
+			// 						   .text()
+			// 						   .trim();
+			// desc = trimNewlines(desc);
+			// var abbr = title.match(/^[A-Z]{1,4}\s[0-9]{2,3}/);
+			// var credits = title.match(/\(([^\)]+)\)/i)[1];
 
 
 			var table = $("#ctl00_ContentPlaceHolder1_SOCListUC1_gvOfferings");
-			console.log('title: ' + name);
+			//console.log($("h3").text().trim());
+			console.log('title: ' + title);
 			console.log('abbr: ' + abbr);
 			console.log('credits: ' + credits);
 			console.log('desc: ' + desc);
 		}
 	});
+}
+
+function parseTitle ($) {
+	var title = $("h3").text().trim();
+	title = title.replace(/(^[A-Z]{1,4}\s[0-9]{2,3})/, '');
+	title = title.replace(/\(([^\)]+)\)/i, '');
+	title = title.replace(/[^\w\s]/gi, '');
+	title = title.replace(/\r?\n|\r|\t/g, '');
+	title = title.replace(/\ {2,}/g, '');
+	return title;
+}
+
+function parseAbbr ($) {
+	return $("h3").text().trim().match(/^[A-Z]{1,4}\s[0-9]{2,3}/);
+}
+
+function parseCredits ($) {
+	return $("h3").text().match(/\(([^\)]+)\)/i)[1];
+}
+
+function parseDesc ($) {
+	var desc = $("#aspnetForm").first()
+							   .clone()
+							   .children()
+							   .remove()
+							   .end()
+							   .text()
+							   .trim();
+	desc = trimNewlines(desc);
+	return desc;
 }
 
 function trimNewlines (desc) {
