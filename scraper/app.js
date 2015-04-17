b3a1950cab6dd362d5afe9061e7847772d1f1336
Rutger Farry <rutgerfarry@gmail.com>
2015-04-16 23:22:56 -0700

    Not using Mongo anymore. DB connection works
    I'm experimenting in saveToDB with felixge's MySQL module. Probably going to use EventEmitters to break up the code, but I don't know those yet, so might just make huge functions for sake of speed, then come back and do it the right way.
    
    Oh I also started using async to reduce the callback confusion in searchParse.
    
    And implemented some style guidelines...
    
    Think thats it. This is a big commit

--- scraper/app.js
+++ scraper/app.js
@@ -1,2 +1,5 @@
 var searchParse = require('./searchParse');
-searchParse.scrape();
+searchParse.scrape(function scrapeComplete (courses) {
+  console.log('Scrape complete. ' + courses.length + 
+              ' course records obtained.');
+});
--- scraper/searchParse.js
+++ scraper/searchParse.js
@@ -1,129 +1,158 @@
 var request = require('request');
 var cheerio = require('cheerio');
-var path    = require('path');
+var async   = require('async');
 
 
 var CATALOG_URL = 'http://catalog.oregonstate.edu/';
 var COURSE_SEARCH_URL = CATALOG_URL + 'CourseSearcher.aspx?chr=abg';
 
 
-function scrapeClassDatabase (url) {
+exports.scrape = getCourseLinks;
 
-	request(url, function parseSearchPage (error, response, body) {
-		if (!error && response.statusCode === 200) {
-			var classURLs = [];
-			$ = cheerio.load(body);
 
-			$("a[id^='ctl00_ContentPlaceHolder']").each(function (i, element) {
-				var link = $(this).attr('href');
-				classURLs.push(link);
-			});
-			return getCourseInfo(CATALOG_URL, classURLs);
-		}
-	});
+/////////////////////////////////////////////////
+// MAIN FUNCTIONS
+/////////////////////////////////////////////////
+
+function getCourseLinks (callback) {
+
+  request(COURSE_SEARCH_URL, function parseSearchPage (err, res, body) {
+    if (!err && res.statusCode === 200) {
+      var classURLs = [];
+      $ = cheerio.load(body);
+
+      $("a[id^='ctl00_ContentPlaceHolder']").each(function (i, element) {
+        var link = $(this).attr('href');
+        classURLs.push(link);
+      });
+      // First two elements are currently trash, don't attempt to scrape
+      classURLs.splice(0, 2);
+      return getCourseInfo(CATALOG_URL, classURLs, callback);
+    }
+  });
 }
 
-function getCourseInfo (baseURL, classURLs) {
-	var courses = [];
-	var classURL = baseURL + classURLs[323];
+function getCourseInfo (baseURL, classURLs, callback) {
+  var courses = [];
+  var index = 1;
 
-	request(classURL, function parseClassPage (error, response, body) {
-		if (!error && response.statusCode === 200) {
-			$ = cheerio.load(body);
+  async.each(classURLs.slice(0, 5), function scrapeClassPages (url, asyncCallback) {
 
-			var course = {};
-			var title   = parseTitle($),
-				abbr    = parseAbbr($),
-				credits = parseCredits($),
-				desc    = parseDesc($),
-				classes = parseTable($);
+    var classURL = baseURL + url;
+    console.log('Scraping ' + index++ + ' of ' + classURLs.length);
+    console.log('URL: ' + classURL);
 
+    request(classURL, function parseClassPage (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        $ = cheerio.load(body);
 
+        var course = {};
+        var title   = parseTitle($),
+          abbr    = parseAbbr($),
+          credits = parseCredits($),
+          desc    = parseDesc($),
+          classes = parseTable($);
 
-			// console.log('title: ' + typeof title);
-			// console.log('abbr: ' + typeof abbr);
-			// console.log('credits: ' + typeof credits);
-			// console.log('desc: ' + typeof desc);
-			// console.log('classes: ' + typeof classes);
-			console.log('title: ' + title);
-			console.log('abbr: ' + abbr);
-			console.log('credits: ' + credits);
-			console.log('desc: ' + desc);
-			console.log('classes: ' + Object.keys(classes[1]));
-		}
-	});
+        console.log('title: ' + title);
+        console.log('abbr: ' + abbr);
+        console.log('credits: ' + credits);
+        console.log('desc: ' + desc);
+        // console.log('classes: ' + Object.keys(classes[1]));
+
+        course.title = title;
+        course.abbr = abbr;
+        course.credits = credits;
+        course.desc = desc;
+        course.classes = classes;
+
+        courses.push(course);
+        asyncCallback();
+      }
+    });
+  }, function (error) {
+    if (error) {
+      console.err('An error occured: ' + error);
+    } else {
+      callback(courses);
+    }
+  });
 }
+
+
+
+/////////////////////////////////////////////////
+// PARSERS
+/////////////////////////////////////////////////
 
 // Parses the table of class meeting times
 function parseTable ($) {
-	var attribs = [];
-	$("th").each(function (i, element) {
-		var attrib = $(this).text().replace(/\r?\n|\r|\t/g, '');
-		attrib = attrib.replace(/\ {2,}/g, '');
-		attribs.push(attrib);
-	});
+  var attribs = [];
+  $('th').each(function (i, element) {
+    var attrib = $(this).text().replace(/\r?\n|\r|\t/g, '');
+    attrib = attrib.replace(/\ {2,}/g, '');
+    attribs.push(attrib);
+  });
 
-	tableRows = $("#ctl00_ContentPlaceHolder1_SOCListUC1_gvOfferings > tr ");
+  tableRows = $('#ctl00_ContentPlaceHolder1_SOCListUC1_gvOfferings > tr ');
 
-	var classes = [];
+  var classes = [];
 
-	tableRows.each(function (i, element) {
-		td = $(this).children("td");
+  tableRows.each(function (i, element) {
+    var classDict = {};
 
-		var classDict = {};
+    td = $(this).children('td');
 
-		td.each(function (i, element) {
-			var data = $(this).text().replace(/\s{2,}/g, ' ').trim();
+    td.each(function (i) {
+      var data = $(this).text().replace(/\s{2,}/g, ' ').trim();
 
-			var key = attribs[i % attribs.length];
-			classDict[key] = data;
-		});
-		classes.push(classDict);
-	});
-	// First element will be categories, so remove it
-	return classes;
+      var key = attribs[i % attribs.length];
+      classDict[key] = data;
+    });
+    classes.push(classDict);
+  });
+  // First element will be categories, so remove it
+  classes.splice(0, 1);
+  return classes;
 }
 
 // Gets the title from the class site
 function parseTitle ($) {
-	var title = $("h3").text().trim();
-	title = title.replace(/(^[A-Z]{1,4}\s[0-9]{2,3})/, '');
-	title = title.replace(/\(([^\)]+)\)/i, '');
-	title = title.replace(/[^\w\s]/gi, '');
-	title = title.replace(/\r?\n|\r|\t/g, '');
-	title = title.replace(/\ {2,}/g, '');
-	return title;
+  var title = $('h3').text().trim();
+  title = title.replace(/(^[A-Z]{1,4}\s[0-9]{2,3})/, '');
+  title = title.replace(/\(([^\)]+)\)/i, '');
+  title = title.replace(/[^\w\s]/gi, '');
+  title = title.replace(/\r?\n|\r|\t/g, '');
+  title = title.replace(/\ {2,}/g, '');
+  return title;
 }
 
 // Gets the abbreviation from the class site
 function parseAbbr ($) {
-	return $("h3").text().trim().match(/^[A-Z]{1,4}\s[0-9]{2,3}/)[0];
+  return $('h3').text().trim().match(/^[A-Z]{1,4}\s[0-9]{2,3}/)[0];
 }
 
 // Gets the credits from the class site
 function parseCredits ($) {
-	return $("h3").text().match(/\(([^\)]+)\)/i)[1];
+  return $('h3').text().match(/\(([^\)]+)\)/i)[1];
 }
 
 // Gets the description from the class site
 function parseDesc ($) {
-	var desc = $("#aspnetForm").first()
-							   .clone()
-							   .children()
-							   .remove()
-							   .end()
-							   .text()
-							   .trim();
-	desc = trimNewlines(desc);
-	return desc;
+  var desc = $('#aspnetForm').first()
+                             .clone()
+                             .children()
+                             .remove()
+                             .end()
+                             .text()
+                             .trim();
+  desc = trimNewlines(desc);
+  return desc;
 }
 
 function trimNewlines (desc) {
-	var n = desc.indexOf('\n');
-	n = n === -1 ? desc.length : n;
-	return desc.substring(0, n);
+  var n = desc.indexOf('\n');
+  n = n === -1 ? desc.length : n;
+  return desc.substring(0, n);
 }
-
-scrapeClassDatabase(COURSE_SEARCH_URL);
 
 
