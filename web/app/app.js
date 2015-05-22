1de9a2d637590cac58412a0cbc139a228246baa1
Rutger Farry <rutgerfarry@gmail.com>
2015-05-22 11:51:53 -0700

    The course detail page is almost MVP-ish

--- web/app/app.js
+++ web/app/app.js
@@ -10,11 +10,4 @@ angular.module('myApp', [
 ])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/'});
-}])
-.run(['$rootScope','$location', '$routeParams', function($rootScope, $location, $routeParams) {
-  $rootScope.$on('$routeChangeSuccess', function(e, current, pre) {
-    console.log('Current route name: ' + $location.path());
-    // Get all URL parameter
-    console.log($routeParams);
-  });
 }]);
